bb39f59a24807da29b39c43d0b42c1d0d00d1f2f
Ravi Kiran C <165673478+RaviKiran2605@users.noreply.github.com>
2024-06-14 19:55:18 +0530

    Add files via upload

---
+++ Pharmacy management system/src/dao/PharmacyUtils.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author Ravi kiran C
+ */
+public class PharmacyUtils {
+
+    /**
+     *
+     */
+    public static String billPath ="lib:\\";
+}
